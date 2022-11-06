4383793e92e2c4742a4b6e480b95c0323ce5404e
lidonglin@rcrai.com <4388193@qq.com>
2022-11-06 11:43:19 +0800

    add int32 & float32 support

--- tcfg_default.go
+++ tcfg_default.go
@@ -29,8 +29,14 @@ var DefaultBool = defaultConfData.DefaultBool
 var Int = defaultConfData.Int
 var DefaultInt = defaultConfData.DefaultInt
 
+var Int32 = defaultConfData.Int32
+var DefaultInt32 = defaultConfData.DefaultInt32
+
 var Int64 = defaultConfData.Int64
 var DefaultInt64 = defaultConfData.DefaultInt64
+
+var Float32 = defaultConfData.Float32
+var DefaultFloat32 = defaultConfData.DefaultFloat32
 
 var Float64 = defaultConfData.Float64
 var DefaultFloat64 = defaultConfData.DefaultFloat64
